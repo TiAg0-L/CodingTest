b0c50175bd417dad1923d8a639070cdc1f600319
TiagoLopes <tiago.lopes.omesmo@gmail.com>
2020-11-14 18:19:27 +0000

    Added Item model.

---
+++ HackerNews.Provider/Models/Item.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace HackerNews.Provider.Models
+{
+    public sealed class Item
+    {
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public string By { get; set; }
+        public string Time { get; set; }
+        public string Descendants { get; set; }
+        public string Score { get; set; }
+        public string Id { get; set; }
+        public IEnumerable<string> Kids { get; set; }
+        public string Type { get; set; }
+    }
+}
