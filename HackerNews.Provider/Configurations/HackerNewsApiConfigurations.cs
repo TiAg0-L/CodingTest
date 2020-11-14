03c738da14e15911081bcc51f5be5db2e1f32464
TiagoLopes <tiago.lopes.omesmo@gmail.com>
2020-11-14 18:18:11 +0000

    Added API configurations.

---
+++ HackerNews.Provider/Configurations/HackerNewsApiConfigurations.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HackerNews.Provider.Configurations
+{
+    /// <summary>
+    /// Contains all configurations required for the Hacker News Api provider.
+    /// </summary>
+    public sealed class HackerNewsApiConfigurations : IHackerNewsApiConfigurations
+    {
+        /// <summary>
+        /// Creas an instance of <see cref="HackerNewsApiConfigurations"/>.
+        /// </summary>
+        /// <param name="basePath">The API base path.</param>
+        public HackerNewsApiConfigurations(string basePath)
+        {
+            BasePath = basePath ?? throw new ArgumentNullException(nameof(basePath));
+        }
+
+        /// <summary>
+        /// Gets the base path.
+        /// </summary>
+        public string BasePath { get; }
+    }
+}
---
+++ HackerNews.Provider/Configurations/IHackerNewsApiConfigurations.cs
@@ -0,0 +1,13 @@
+﻿namespace HackerNews.Provider.Configurations
+{
+    /// <summary>
+    /// Contains all configurations required for the Hacker News Api provider.
+    /// </summary>
+    public interface IHackerNewsApiConfigurations
+    {
+        /// <summary>
+        /// Gets the base path.
+        /// </summary>
+        string BasePath { get; }
+    }
+}
