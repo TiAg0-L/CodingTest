53f319018925d1ff678b930aeeef4ae670cc78aa
TiagoLopes <tiago.lopes.omesmo@gmail.com>
2020-11-15 13:48:44 +0000

    Added domain level tests.

---
+++ CodingTest.DomainTests/Stories/StoriesTests.cs
@@ -0,0 +1,96 @@
+﻿using CodingTest.Domain.Stories;
+using HackerNews.Provider;
+using HackerNews.Provider.Models;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using DomainStories = CodingTest.Domain.Stories;
+
+namespace CodingTest.DomainTests.Stories
+{
+    [TestFixture]
+    public class StoriesTests
+    {
+        [Test]
+        public void StoriesTest_GivenNullAPI_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+                new DomainStories.Stories(api: null)
+            );
+        }
+
+        [Test]
+        public void Get20BestStoriesTest_Given1Story_ReturnsExpectedStory()
+        {
+            var item = new Item
+            {
+                Title = "A title",
+                Url = "https://www.example.com",
+                By = "An author",
+                Time = "1",
+                Descendants = "2",
+                Score = 3,
+                Id = "4",
+                Kids = new string[] { "5", "6" },
+                Type = "story"
+            };
+            var expectedStory = new Story(
+                title: item.Title,
+                uri: item.Url,
+                postedBy: item.By,
+                time: item.Time,
+                score: item.Score,
+                commentCount: item.Kids.Count()
+            );
+            var apiMock = new Mock<IHackerNewsAPI>();
+            apiMock.Setup((m) => m.GetBeststories()).Returns(new string[] { item.Id });
+            apiMock.Setup((m) => m.GetItem(item.Id)).Returns(item);
+
+            var result = new DomainStories.Stories(api: apiMock.Object).Get20BestStories();
+            var firstResult = result.First();
+
+            Assert.AreEqual(1, result.Count());
+            Assert.AreEqual(expectedStory.CommentCount, firstResult.CommentCount);
+            Assert.AreEqual(expectedStory.PostedBy, firstResult.PostedBy);
+            Assert.AreEqual(expectedStory.Score, firstResult.Score);
+            Assert.AreEqual(expectedStory.Time, firstResult.Time);
+            Assert.AreEqual(expectedStory.Title, firstResult.Title);
+            Assert.AreEqual(expectedStory.Uri, firstResult.Uri);
+        }
+
+        [Test]
+        public void Get20BestStoriesTest_Given21Stories_Returns20StoriesWithBestScore()
+        {
+            var apiMock = new Mock<IHackerNewsAPI>();
+            var items = new List<Item>();
+            for (int i = 0; i < 21; i++)
+            {
+                var item =
+                   new Item
+                   {
+                       Title = "A title",
+                       Url = "https://www.example.com",
+                       By = "An author",
+                       Time = "1",
+                       Descendants = "2",
+                       Score = i,
+                       Id = i.ToString(),
+                       Kids = new string[] { "5", "6" },
+                       Type = "story"
+                   };
+                items.Add(item);
+
+                apiMock.Setup((m) => m.GetItem(item.Id)).Returns(item);
+
+            }
+            apiMock.Setup((m) => m.GetBeststories()).Returns(items.Select(item => item.Id));
+
+            var result = new DomainStories.Stories(api: apiMock.Object).Get20BestStories();
+
+            Assert.AreEqual(20, result.Count());
+            Assert.That(result, Has.No.Member(items.Where(item => item.Score == 0)));
+        }
+    }
+}
---
+++ CodingTest.DomainTests/Stories/StoryTests.cs
@@ -0,0 +1,74 @@
+﻿using CodingTest.Domain.Stories;
+using NUnit.Framework;
+using System;
+
+namespace CodingTest.DomainTests.Stories
+{
+    [TestFixture]
+    public class StoryTests
+    {
+        [Test]
+        public void TimeTest_GivenNullTime_ReturnsNull()
+        {
+            var result = new Story(
+                title: null,
+                uri: null,
+                postedBy: null,
+                time: null,
+                score: 0,
+                commentCount: 0
+            ).Time;
+
+            Assert.IsNull(result);
+        }
+
+        [Test]
+        public void TimeTest_GivenEmptyTimeString_ReturnsNull()
+        {
+            var result = new Story(
+                title: null,
+                uri: null,
+                postedBy: null,
+                time: null,
+                score: 0,
+                commentCount: 0
+            ).Time;
+
+            Assert.IsNull(result);
+        }
+
+        [Test]
+        public void TimeTest_GivenWhiteTimeString_ReturnsNull()
+        {
+            var result = new Story(
+                title: null,
+                uri: null,
+                postedBy: null,
+                time: null,
+                score: 0,
+                commentCount: 0
+            ).Time;
+
+            Assert.IsNull(result);
+        }
+
+        [Test]
+        public void TimeTest_GivenSecondsFromUnixEpochToNow_ReturnsCorrectValue()
+        {
+            var expectedDateTime = new DateTime(2020,1,1,1,1,1,DateTimeKind.Utc);
+            var totalSeconds = (expectedDateTime - new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc))
+                .TotalSeconds;
+
+            var result = new Story(
+                title: null,
+                uri: null,
+                postedBy: null,
+                time: totalSeconds.ToString(),
+                score: 0,
+                commentCount: 0
+            ).Time;
+
+            Assert.AreEqual(expectedDateTime, result);
+        }
+    }
+}
