9273b51b3010c2dfc7649f5dbac09e4a0a3897f0
TiagoLopes <tiago.lopes.omesmo@gmail.com>
2020-11-15 12:02:42 +0000

    Aligned configuration model

--- HackerNews.Provider/Configurations/HackerNewsApiConfigurations.cs
+++ HackerNews.Provider/Configurations/HackerNewsApiConfigurations.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace HackerNews.Provider.Configurations
+﻿namespace HackerNews.Provider.Configurations
 {
     /// <summary>
     /// Contains all configurations required for the Hacker News Api provider.
@@ -8,17 +6,8 @@ namespace HackerNews.Provider.Configurations
     public sealed class HackerNewsApiConfigurations : IHackerNewsApiConfigurations
     {
         /// <summary>
-        /// Creas an instance of <see cref="HackerNewsApiConfigurations"/>.
-        /// </summary>
-        /// <param name="basePath">The API base path.</param>
-        public HackerNewsApiConfigurations(string basePath)
-        {
-            BasePath = basePath ?? throw new ArgumentNullException(nameof(basePath));
-        }
-
-        /// <summary>
         /// Gets the base path.
         /// </summary>
-        public string BasePath { get; }
+        public string BasePath { get; set; }
     }
 }
